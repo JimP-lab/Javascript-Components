e5a461964151aeb3e78e6895eabdcb2904da4156
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-06-11 02:52:07 +0300

    Update and rename ask1.html to ask1.js

--- ask1.js
+++ ask1.js
@@ -4,8 +4,6 @@
 		<title> Παραδειγμα 1 JS </title> 
 		<meta charset="utf-8"> 
 	</head> 
-	<body> 
-		<p> Παράδειγμα 1 χρήσης javascript!</p> 
 		<script> 
 			document.write(1 + 2); 
 			window.alert(2 + 3); 
