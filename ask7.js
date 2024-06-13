681956a8220b86eebade64ca2bb232713c5d9bcd
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-06-13 19:45:28 +0300

    Update ask7.js

--- ask7.js
+++ ask7.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html> 
-<html> 
-	<head> 
-		<title> Παραδειγμα 7 JS </title> 
-		<meta charset="utf-8"> 
-	</head> 
-
-	<body> 
-		<p>Παράδειγμα 7 χρήσης javascript!</p> 
-		 
-		<script>
 			var eisodosXristi; eisodosXristi = prompt("Dwse timi"); 
 			var eisodosXristiAkeraios = parseInt(eisodosXristi); 
 			var eisodosXristiDekadikos = parseFloat(eisodosXristi); 
@@ -20,6 +9,3 @@
 			document.write(eisodosXristiAkeraios-1);  
 			window.alert(eisodosXristiAkeraios-1); 
 			console.log(eisodosXristiAkeraios-1); 
-		</script> 
-	</body> 
-</html> 
