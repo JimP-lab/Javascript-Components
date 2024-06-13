2438c14d54ef33491b7509c1df7d400762b886ce
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-06-13 19:39:03 +0300

    Update ask1.js

--- ask1.js
+++ ask1.js
@@ -1,19 +1,7 @@
-<!DOCTYPE html> 
-<html> 
-	<head> 
-		<title> Παραδειγμα 1 JS </title> 
-		<meta charset="utf-8"> 
-	</head> 
-		<script> 
+
 			document.write(1 + 2); 
 			window.alert(2 + 3); 
 			console.log(4 + 5); 
-		</script>
-
-		<script> 
 			document.write("Alex-2024"); 
 			window.alert("Μάθημα"); 				
 			console.log("9β"); 
-		</script> 
-	</body> 
-</html> 
